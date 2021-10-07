6c58f0dffd8d42806a9aaf69521242296c08d80f
sash <softester@yandex.ru>
2021-10-08 02:21:26 +0300

    Запросы вынесены в отдельную библиотеку

--- tests/test_user_auth.py
+++ tests/test_user_auth.py
@@ -1,7 +1,7 @@
-import requests
 import pytest
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
+from lib.my_requests import MyRequests
 
 
 class TestUserAuth(BaseCase):
@@ -17,15 +17,15 @@ class TestUserAuth(BaseCase):
             "password": "1234"
         }
 
-        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
+        response1 = MyRequests.post("/user/login", data=data)
 
         self.auth_sid = self.get_cookie(response1, "auth_sid")
         self.token = self.get_header(response1, "x-csrf-token")
         self.user_id = self.get_json_value(response1, "user_id")
 
     def test_auth_user(self):
-        response2 = requests.get(
-            "https://playground.learnqa.ru/api/user/auth",
+        response2 = MyRequests.get(
+            "/user/auth",
             headers={"x-csrf-token": self.token},
             cookies={"auth_sid": self.auth_sid}
         )
@@ -40,10 +40,10 @@ class TestUserAuth(BaseCase):
     @pytest.mark.parametrize("condition", exclude_params)
     def test_negative_auth_check(self, condition):
         if condition == "no_cookie":
-            response2 = requests.get("https://playground.learnqa.ru/api/user/auth",
+            response2 = MyRequests.get("/user/auth",
                                      headers={"x-csrf-token": self.token})
         else:
-            response2 = requests.get("https://playground.learnqa.ru/api/user/auth",
+            response2 = MyRequests.get("/user/auth",
                                      cookies={"auth_sid": self.auth_sid})
 
         Assertions.assert_json_value_by_name(
--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -1,4 +1,4 @@
-import requests
+from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 
@@ -6,7 +6,7 @@ class TestUserEdit(BaseCase):
     def test_edit_just_created_user(self):
         # REGISTER
         register_data = self.prepare_registration_data()
-        response1 = requests.post("https://playground.learnqa.ru/api/user/", data=register_data)
+        response1 = MyRequests.post("/user/", data=register_data)
 
         Assertions.assert_code_status(response1, 200)
         Assertions.assert_json_has_key(response1, "id")
@@ -21,21 +21,21 @@ class TestUserEdit(BaseCase):
             "email": email,
             "password": password
         }
-        response2 = requests.post("https://playground.learnqa.ru/api/user/login", data=login_data)
+        response2 = MyRequests.post("/user/login", data=login_data)
         auth_sid = self.get_cookie(response2, "auth_sid")
         token = self.get_header(response2, "x-csrf-token")
 
         # EDIT
-        new_name = "Cjanged name"
+        new_name = "Changed name"
 
-        response3 = requests.put(f"https://playground.learnqa.ru/api/user/{user_id}",
+        response3 = MyRequests.put(f"/user/{user_id}",
                                  cookies={"auth_sid": auth_sid},
                                  headers={"x-csrf-token": token},
                                  data={"firstName": new_name})
 
         Assertions.assert_code_status(response3, 200)
 
-        response4 = requests.get(f"https://playground.learnqa.ru/api/user/{user_id}",
+        response4 = MyRequests.get(f"/user/{user_id}",
                                  cookies={"auth_sid": auth_sid},
                                  headers={"x-csrf-token": token})
 

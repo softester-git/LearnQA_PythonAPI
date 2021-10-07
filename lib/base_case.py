627c0e5b1425c632fd9b773f342b0c581d7d3d3a
sash <softester@yandex.ru>
2021-10-07 23:52:28 +0300

    Добавлены тесты проверки пользователя

--- lib/base_case.py
+++ lib/base_case.py
@@ -1,5 +1,5 @@
 import json.decoder
-
+from datetime import datetime
 from requests import Response
 
 
@@ -21,3 +21,17 @@ class BaseCase:
         assert name in response_as_dict, f"Response json does not have key '{name}'"
 
         return response_as_dict[name]
+
+    def prepare_registration_data(self, email=None):
+        if email is None:
+            base_part = "learnqa"
+            domain = "example.com"
+            random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+            email = f"{base_part}{random_part}@{domain}"
+        return {
+            "password": "123",
+            "username": "learnqa",
+            "firstName": "learnqa",
+            "lastName": "learnqa",
+            "email": email
+        }

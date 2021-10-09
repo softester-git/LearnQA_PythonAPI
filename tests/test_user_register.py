07fc2c1ce1b72c6ec7785c804c87a3cd5f848037
sash <softester@yandex.ru>
2021-10-09 03:40:35 +0300

    Аннотации allure для test_user_register.py

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,11 +1,15 @@
+import allure
+
 from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 import pytest
 
-
+@allure.epic("Create cases")
 class TestUserRegister(BaseCase):
-
+    @allure.epic("Create cases")
+    @allure.feature("Register users")
+    @allure.story("Register with incorrect email")
     def test_create_test_user_with_incorrect_email(self):
         email = "test.com"
         data = self.prepare_registration_data(email=email)
@@ -51,11 +55,17 @@ class TestUserRegister(BaseCase):
         }
     ]
 
+    @allure.epic("Create cases")
+    @allure.feature("Register users")
+    @allure.story("Register with missing some fields")
     @pytest.mark.parametrize("data", data)
     def test_create_user_wo_some_fields(self, data):
         response = MyRequests.post("/user/", data=data)
         Assertions.assert_code_status(response, 400)
 
+    @allure.epic("Create cases")
+    @allure.feature("Register users")
+    @allure.story("Register with 1 symbol in firstname")
     def test_create_user_with_short_name(self):
         data = self.prepare_registration_data()
         data["firstName"] = "a"
@@ -63,6 +73,9 @@ class TestUserRegister(BaseCase):
         Assertions.assert_code_status(response, 400)
         assert response.content.decode("utf-8") == f"The value of 'firstName' field is too short"
 
+    @allure.epic("Create cases")
+    @allure.feature("Register users")
+    @allure.story("Register with 251 symbols in firstname")
     def test_create_user_with_long_name(self):
         data = self.prepare_registration_data()
         data["firstName"] = "a"*251
@@ -73,15 +86,20 @@ class TestUserRegister(BaseCase):
         assert response.content.decode("utf-8") == f"The value of 'firstName' field is too long"
 
 
-
-
-
+    @allure.epic("Create cases")
+    @allure.feature("Register users")
+    @allure.story("Register user successfully")
+    @allure.description("Test cases from video lessons")
     def test_create_user_successfully(self):
         data = self.prepare_registration_data()
         response = MyRequests.post("/user/", data=data)
         Assertions.assert_code_status(response, 200)
         Assertions.assert_json_has_key(response, "id")
 
+    @allure.epic("Create cases")
+    @allure.feature("Register users")
+    @allure.story("Register existing user")
+    @allure.description("Test cases from video lessons")
     def test_create_user_with_existing_email(self):
         email = "vinkotov@example.com"
         data = self.prepare_registration_data(email=email)

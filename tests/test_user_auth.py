a9d60c191a132aba8451acc6bfcebb3ec2be2fcf
sash <softester@yandex.ru>
2021-10-09 03:09:28 +0300

    Аннотации allure для test_user_auth.py

--- tests/test_user_auth.py
+++ tests/test_user_auth.py
@@ -12,6 +12,9 @@ class TestUserAuth(BaseCase):
         ("no_token")
     ]
 
+    @allure.epic("Authrization cases")
+    @allure.feature("Login method")
+    @allure.story("Get parameters on login")
     def setup(self):
         data = {
             "email": "vinkotov@example.com",
@@ -24,6 +27,9 @@ class TestUserAuth(BaseCase):
         self.token = self.get_header(response1, "x-csrf-token")
         self.user_id = self.get_json_value(response1, "user_id")
 
+    @allure.epic("Authrization cases")
+    @allure.feature("Auth method")
+    @allure.story("Assert user_id authorized user")
     @allure.description("This test successfully authorize user by email and password")
     def test_auth_user(self):
         response2 = MyRequests.get(
@@ -39,6 +45,9 @@ class TestUserAuth(BaseCase):
             "User id in auth method is not equal to user id from check method"
         )
 
+    @allure.epic("Authrization cases")
+    @allure.feature("Auth method")
+    @allure.story("Denie on empty cookies or headers")
     @allure.description("This test checks authorization status w/o sending auth cookies or token")
     @pytest.mark.parametrize("condition", exclude_params)
     def test_negative_auth_check(self, condition):
